79f3b7ba90e7100bd6de6c71dc1891cedbe96094
cleberlucas <clebersiber@hotmail.com>
2023-12-29 00:33:38 -0300

    adjusting return function name

--- contracts/IRead.sol
+++ contracts/IRead.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.23;
 import "./DelimitationLib.sol";
 
 interface IRead{
-    function ArticleIds(uint256 startIndex, uint256 endIndex, bool reverse) external view returns (bytes32[] memory result, uint256 size);
+    function ArticleIds(uint256 startIndex, uint256 endIndex, bool reverse) external view returns (bytes32[] memory result, uint256 currentSize);
     function ArticlePoster(bytes32[] memory articleIds) external view returns (address[] memory result);
     function ArticleInstitution(bytes32[] memory articleIds) external view returns (address[] memory result);
     function ArticleContent(bytes32[] memory articleIds) external view returns (DelimitationLib.Article[] memory result);
-    function InstitutionAccounts(uint256 startIndex, uint256 endIndex, bool reverse) external view returns (address[] memory result, uint256 size);
-    function InstitutionAuthenticators(address[] memory institutionAccounts, uint256 startIndex, uint256 endIndex, bool reverse) external view returns (address[][] memory result, uint256[] memory size);
+    function InstitutionAccounts(uint256 startIndex, uint256 endIndex, bool reverse) external view returns (address[] memory result, uint256 currentSize);
+    function InstitutionAuthenticators(address[] memory institutionAccounts, uint256 startIndex, uint256 endIndex, bool reverse) external view returns (address[][] memory result, uint256[] memory currentSize);
 }
