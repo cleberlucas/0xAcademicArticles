32ee667628bdeed1dac3568cd37dfac40843eba8
cleberlucas <clebersiber@hotmail.com>
2023-12-30 14:52:32 -0300

    ajusting message error

--- contracts/MessageLib.sol
+++ contracts/MessageLib.sol
@@ -11,15 +11,15 @@ library MessageLib {
     string public constant ONE_OF_ACCOUNTS_IS_EMPTY = "one-of-accounts-is-empty";
     string public constant ONE_OF_ACCOUNTS_IS_DUPLICATED = "one-of-accounts-is-duplicated";
     string public constant ONE_OF_ARTICLES_IS_DUPLICATED = "one-of-articles-is-duplicated";
-    string public constant ONE_OF_ARTICLES_ALREADY_EXIST = "one-of-articles-already-published";
-    string public constant ONE_OF_ARTICLES_WAS_NOT_EXIST = "one-of-articles-was-not-published";
+    string public constant ONE_OF_ARTICLES_ALREADY_EXIST = "one-of-articles-already-exist";
+    string public constant ONE_OF_ARTICLES_WAS_NOT_EXIST = "one-of-articles-was-not-exist";
     string public constant ONE_OF_ARTICLES_ALREADY_VALIDATED = "one-of-articles-already-validated";
     string public constant ONE_OF_ARTICLES_WAS_NOT_VALIDATED = "one-of-articles-was-not-validated";
     string public constant ONE_OF_ARTICLES_NOT_YOURS = "one-of-articles-not-yours";
-    string public constant ONE_OF_AFFILIATES_WAS_NOT_EXIST = "one-of-affiliates-was-not-registered";
-    string public constant ONE_OF_AFFILIATES_ALREADY_EXIST= "one-of-affiliates-already-registered";
-    string public constant ONE_OF_INSTITUTIONS_WAS_NOT_EXIST = "one-of-institutionswas-not-registered";
-    string public constant ONE_OF_INSTITUTIONS_ALREADY_EXIST= "one-of-institutions-already-registered";
+    string public constant ONE_OF_AFFILIATES_WAS_NOT_EXIST = "one-of-affiliates-was-not-exist";
+    string public constant ONE_OF_AFFILIATES_ALREADY_EXIST= "one-of-affiliates-already-exist";
+    string public constant ONE_OF_INSTITUTIONS_WAS_NOT_EXIST = "one-of-institutions-was-not-exist";
+    string public constant ONE_OF_INSTITUTIONS_ALREADY_EXIST= "one-of-institutions-already-exist";
     string public constant ONE_OF_AFFILIATES_WAS_NOT_LINKED_IN_INSTITUTION = "one-of-affiliates-was-not-linked-in-institution";
     string public constant ONE_OF_THE_ARTICLES_WAS_NOT_VALIDATED_BY_INSTITUTION = "one-of-the-articles-was-not-validated-by-institution";
 }
