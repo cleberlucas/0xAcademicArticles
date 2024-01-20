2bbb7539e8e5bae94475ec0ac56fbd182e367335
cleberlucas <clebersiber@hotmail.com>
2024-01-20 19:58:24 -0300

    ajusting messages

--- contracts/main/libs/ERCXMessage.sol
+++ contracts/main/libs/ERCXMessage.sol
@@ -7,7 +7,7 @@ library ERCXMessage {
     string public constant SIGNATURE_EMPTY = "signature is empty";
     string public constant SENDER_ALREADY_SIGNED = "sender is already signed";
     string public constant OTHER_SENDER_IS_USING_THIS_SIGNATURE = "other sender is using this signature";
-    string public constant METADATA_IS_ALREADY_SENDED = "metadata is already sent";
-    string public constant METADATA_IS_NOT_SENDED = "metadata is not sent";
+    string public constant METADATA_IS_ALREADY_SENDED = "metadata is already sended";
+    string public constant METADATA_IS_NOT_SENDED = "metadata is not sended";
     string public constant METADATA_IS_NOT_SENDED_BY_YOU = "metadata is not sent by you";
 }
