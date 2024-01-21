8487313b40de7793628bf33648ef54511f73123d
cleberlucas <clebersiber@hotmail.com>
2024-01-21 12:13:56 -0300

    adding treatments to sell that do not have a signature implemented

--- contracts/AIO/AIORules.sol
+++ contracts/AIO/AIORules.sol
@@ -9,34 +9,50 @@ import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 abstract contract AIORules  {
     modifier InitializeRule(AIOStorageModel.Interconnection storage _interconnection) {
-        require(address(this) != msg.sender, AIOMessage.AIO_NOT_EXEC);
-        require(bytes(IAIOSignature(msg.sender).SIGNATURE()).length > 0, AIOMessage.SIGNATURE_EMPTY);
-        require(bytes(_interconnection.signature[msg.sender]).length == 0, AIOMessage.SENDER_ALREADY_SIGNED);
-        require(_interconnection.sender[IAIOSignature(msg.sender).SIGNATURE()] == address(0), AIOMessage.OTHER_SENDER_USING_SIGNATURE);
+        address sender = msg.sender;
+
+        require(address(this) != sender, AIOMessage.AIO_NOT_EXEC);
+        try IAIOSignature(sender).SIGNATURE() {
+            require(bytes(IAIOSignature(sender).SIGNATURE()).length > 0, AIOMessage.SIGNATURE_EMPTY);
+            require(bytes(_interconnection.signature[sender]).length == 0, AIOMessage.SENDER_ALREADY_SIGNED);
+            require(_interconnection.sender[IAIOSignature(sender).SIGNATURE()] == address(0), AIOMessage.OTHER_SENDER_USING_SIGNATURE);
+        }   catch {
+                revert(AIOMessage.SENDER_NO_SIGNATURE);
+        } 
         _;
     }
 
     modifier TransferSignatureRule(AIOStorageModel.Interconnection storage _interconnection, address newSender) {
-        require(address(this) != msg.sender, AIOMessage.AIO_NOT_EXEC);
-        require(bytes(_interconnection.signature[msg.sender]).length > 0, AIOMessage.ONLY_SIGNED_EXEC);
-        require(newSender != msg.sender, AIOMessage.NEW_SENDER_CANNOT_BE_YOU);
-        require(Strings.equal(IAIOSignature(msg.sender).SIGNATURE(), IAIOSignature(newSender).SIGNATURE()), AIOMessage.NEW_SENDER_NO_SAME_SIGNATURE);
+        address oldSender = msg.sender;
+
+        require(address(this) != oldSender, AIOMessage.AIO_NOT_EXEC);
+        require(bytes(_interconnection.signature[oldSender]).length > 0, AIOMessage.ONLY_SIGNED_EXEC);
+        require(newSender != oldSender, AIOMessage.NEW_SENDER_CANNOT_BE_YOU); 
+        try IAIOSignature(newSender).SIGNATURE() {
+            require(Strings.equal(IAIOSignature(oldSender).SIGNATURE(), IAIOSignature(newSender).SIGNATURE()), AIOMessage.NEW_SENDER_NO_SAME_SIGNATURE);
+        }   catch {
+                revert(AIOMessage.NEW_SENDER_NO_SIGNATURE);
+        }    
         _;
     }
 
     modifier SendMetaDataRule(AIOStorageModel.Interconnection storage _interconnection, AIOStorageModel.Data storage _data, bytes calldata metadata) {
-        require(address(this) != msg.sender, AIOMessage.AIO_NOT_EXEC);
-        require(bytes(_interconnection.signature[msg.sender]).length > 0, AIOMessage.ONLY_SIGNED_EXEC);
+        address sender = msg.sender;
+
+        require(address(this) != sender, AIOMessage.AIO_NOT_EXEC);
+        require(bytes(_interconnection.signature[sender]).length > 0, AIOMessage.ONLY_SIGNED_EXEC);
         require(metadata.length > 0, AIOMessage.METADATA_EMPTY);
         require(bytes(_data.signature[keccak256(metadata)]).length == 0, AIOMessage.METADATA_ALREADY_SENT);
         _;
     }
 
     modifier CleanMetaDataRule(AIOStorageModel.Interconnection storage _interconnection, AIOStorageModel.Data storage _data, bytes32 token) {
-        require(address(this) != msg.sender, AIOMessage.AIO_NOT_EXEC);
-        require(bytes(_interconnection.signature[msg.sender]).length > 0, AIOMessage.ONLY_SIGNED_EXEC);
+        address sender = msg.sender;
+        
+        require(address(this) != sender, AIOMessage.AIO_NOT_EXEC);
+        require(bytes(_interconnection.signature[sender]).length > 0, AIOMessage.ONLY_SIGNED_EXEC);
         require(bytes(_data.signature[token]).length > 0, AIOMessage.METADATA_NOT_SENT);
-        require(Strings.equal((_data.signature[token]), _interconnection.signature[msg.sender]), AIOMessage.METADATA_NOT_SENT_BY_YOU);
+        require(Strings.equal((_data.signature[token]), _interconnection.signature[sender]), AIOMessage.METADATA_NOT_SENT_BY_YOU);
         _;
     }
 }
--- contracts/AIO/libs/AIOMessage.sol
+++ contracts/AIO/libs/AIOMessage.sol
@@ -3,15 +3,16 @@ pragma solidity ^0.8.23;
 
 library AIOMessage {
     string public constant AIO_NOT_EXEC = "AIO: do not execute this action";
-    string public constant ONLY_SIGNED_EXEC = "Only sender signed can execute this action";
-    string public constant METADATA_EMPTY = "Metadata is empty";
-    string public constant SIGNATURE_EMPTY = "Signature is empty";
-    string public constant NEW_SENDER_CANNOT_BE_YOU = "New sender cannot be you";
-    string public constant NEW_SENDER_NO_SAME_SIGNATURE = "New sender does not have same signature as you";
-    string public constant SENDER_ALREADY_SIGNED = "Sender is already signed";
-    string public constant OTHER_SENDER_USING_SIGNATURE = "Other sender is using this signature";
     string public constant METADATA_ALREADY_SENT = "Metadata is already sent";
+    string public constant METADATA_EMPTY = "Metadata is empty";
     string public constant METADATA_NOT_SENT = "Metadata is not sent";
     string public constant METADATA_NOT_SENT_BY_YOU = "Metadata is not sent by you";
+    string public constant NEW_SENDER_CANNOT_BE_YOU = "New sender cannot be you";
+    string public constant NEW_SENDER_NO_SAME_SIGNATURE = "New sender does not have the same signature as you";
+    string public constant NEW_SENDER_NO_SIGNATURE = "New sender does not have a signature";
+    string public constant ONLY_SIGNED_EXEC = "Only sender signed can execute this action";
+    string public constant OTHER_SENDER_USING_SIGNATURE = "Other sender is using this signature";
+    string public constant SENDER_ALREADY_SIGNED = "Sender is already signed";
+    string public constant SENDER_NO_SIGNATURE = "Sender does not have a signature";
+    string public constant SIGNATURE_EMPTY = "Signature is empty";
 }
-
