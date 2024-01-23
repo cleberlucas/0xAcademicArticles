c078d48f4c83ef5cbce17e3d43239f232cc7c369
cleberlucas <clebersiber@hotmail.com>
2024-01-23 12:30:05 -0300

    creating a storage model and own instance for the AIO contract, and leaving the search functions only for the business model, removing the constructor due to incompatibility issues when checking the code in the scan

--- OpenAcademicArticles/contracts/OpenAcademicArticles.sol
+++ OpenAcademicArticles/contracts/OpenAcademicArticles.sol
@@ -41,41 +41,30 @@ contract OpenAcademicArticles is IAIOSignature {
     struct Publication_StorageModel {
         bytes32[] identifications;
         address[] publishers;
-		mapping(bytes32 identification => address) publisher;
+        mapping(bytes32 identification => address) publisher;
         mapping(bytes32 identification => uint) datetime;
         mapping(bytes32 identification => uint) blockNumber;
-		mapping(address publisher => bytes32[]) identificationsOfPublisher;
+        mapping(address publisher => bytes32[]) identificationsOfPublisher;
+    }
+
+    struct AIO_StorageModel {     
+        address account;
+        IAIOSearch articlesSearch;
+        IAIOInteract articlesInteract;
     }
 
     event ArticlesPublished(bytes32[] indexed publicationIdentifications);
     event ArticlesUnpublished(bytes32[] indexed publicationIdentifications);
 
-    bool connectedInAIO;
-
-    address immutable AIO;
-    IAIOSearch internal immutable _articlesSearch;
-    IAIOInteract internal immutable _articlesInteract;
-
     Publication_StorageModel internal _publication;
-
-    constructor(address aio) {
-        AIO = aio;
-        _articlesSearch = IAIOSearch(AIO);
-        _articlesInteract = IAIOInteract(AIO);
-    }
-    
-    function ConnectToAIO() 
+    AIO_StorageModel internal _aio;
+  
+    function InitializeAIO(address account) 
     external payable {
-        require(!connectedInAIO, "Already connected to the AIO");
-
-        IAIOInterconnection(AIO).Initialize();
-
-        connectedInAIO = true;
-    }
-
-    function TransferSignature(address newSender) 
-    external payable {
-        IAIOInterconnection(AIO).TransferSignature(newSender);
+        IAIOInterconnection(account).Initialize();
+        _aio.articlesSearch = IAIOSearch(account);
+        _aio.articlesInteract = IAIOInteract(account);
+        _aio.account = account;
     }
 
     function SIGNATURE() 
@@ -84,29 +73,16 @@ contract OpenAcademicArticles is IAIOSignature {
         signature = "OpenAcademicArticles";
     }
 
-    function PublicationIdentifications() 
-    external view 
-    returns (bytes32[] memory publicationIdentifications) {
-        publicationIdentifications = _publication.identifications;
-    }
-
-    function PublicationPublishers() 
-    external view 
-    returns (address[] memory publicationPublishers) {
-        publicationPublishers = _publication.publishers;
-    }
-
-    function PublicationIdentificationsOfPublisher(address publicationPublisher) 
-    external view 
-    returns (bytes32[] memory publicationIdentificationsOfPublisher) {
-        publicationIdentificationsOfPublisher = _publication.identificationsOfPublisher[publicationPublisher];
+    function TransferSignature(address newSender) 
+    external payable {
+        IAIOInterconnection(_aio.account).TransferSignature(newSender);
     }
 
     function Publication(bytes32 publicationIdentification) 
     external view 
     returns (Publication_Model memory publication) {
         publication = Publication_Model(
-            abi.decode(_articlesSearch.MetaData(publicationIdentification), (Article_Model)),
+            abi.decode(_aio.articlesSearch.MetaData(publicationIdentification), (Article_Model)),
             _publication.publisher[publicationIdentification],
             _publication.datetime[publicationIdentification],
             _publication.blockNumber[publicationIdentification]
@@ -128,7 +104,7 @@ contract OpenAcademicArticles is IAIOSignature {
                 
                 for (uint256 i = 0; i < size; i++) {
                     publicationsPreview[i] = PublicationPreview_Model(
-                        abi.decode(_articlesSearch.MetaData(_publication.identifications[startIndex + i]), (Article_Model)).title,
+                        abi.decode(_aio.articlesSearch.MetaData(_publication.identifications[startIndex + i]), (Article_Model)).title,
                         _publication.identifications[startIndex + i]
                     );
                 }
@@ -150,7 +126,7 @@ contract OpenAcademicArticles is IAIOSignature {
                 
                 for (uint256 i = 0; i < size; i++) {
                     previewPublicationsOfPublisher[i] = PublicationPreview_Model(
-                        abi.decode(_articlesSearch.MetaData(_publication.identificationsOfPublisher[publicationPublisher][startIndex + i]), (Article_Model)).title,
+                        abi.decode(_aio.articlesSearch.MetaData(_publication.identificationsOfPublisher[publicationPublisher][startIndex + i]), (Article_Model)).title,
                         _publication.identificationsOfPublisher[publicationPublisher][startIndex + i]
                     );
                 }
@@ -165,7 +141,7 @@ contract OpenAcademicArticles is IAIOSignature {
         for (uint256 i = 0; i < articles.length; i++) {
             Article_Model memory article = articles[i];
 
-            try _articlesInteract.SendMetaData(abi.encode(article)) {
+            try _aio.articlesInteract.SendMetaData(abi.encode(article)) {
                 bytes32 publicationIdentification = keccak256(abi.encode(article));
 
                 _publication.identifications.push(publicationIdentification);
@@ -196,39 +172,39 @@ contract OpenAcademicArticles is IAIOSignature {
         for (uint256 i = 0; i < publicationIdentifications.length; i++) {
             bytes32 publicationIdentification = publicationIdentifications[i];
             address publisher = _publication.publisher[publicationIdentification];
+            require (publisher != address(0), "Article is not published");
             require (publisher == msg.sender, "Article is not published by you");
 
-            try _articlesInteract.CleanMetaData(publicationIdentification) {
-                if (publisher != address(0)) {
-                    _publication.publisher[publicationIdentification] = address(0);
-                    _publication.datetime[publicationIdentification] = 0;
-                    _publication.blockNumber[publicationIdentification] = 0;
+            try _aio.articlesInteract.CleanMetaData(publicationIdentification) {
+                _publication.publisher[publicationIdentification] = address(0);
+                _publication.datetime[publicationIdentification] = 0;
+                _publication.blockNumber[publicationIdentification] = 0;
 
-                    for (uint256 ii = 0; ii < _publication.identifications.length; ii++) {
-                        if (_publication.identifications[ii] == publicationIdentification) {
-                            _publication.identifications[ii] = _publication.identifications[_publication.identifications.length - 1];
-                            _publication.identifications.pop();             
+                for (uint256 ii = 0; ii < _publication.identifications.length; ii++) {
+                    if (_publication.identifications[ii] == publicationIdentification) {
+                        _publication.identifications[ii] = _publication.identifications[_publication.identifications.length - 1];
+                        _publication.identifications.pop();             
+                    }
+                }
+
+                if (_publication.identificationsOfPublisher[publisher].length == 1) {
+                    _publication.identificationsOfPublisher[publisher] = new bytes32[](0);
+
+                    for (uint256 ii = 0; ii < _publication.publishers.length; ii++) {
+                        if (_publication.publishers[ii] == publisher) {
+                            _publication.publishers[ii] = _publication.publishers[_publication.publishers.length - 1];
+                            _publication.publishers.pop();             
                         }
                     }
-
-                    if (_publication.identificationsOfPublisher[publisher].length == 1) {
-                        _publication.identificationsOfPublisher[publisher] = new bytes32[](0);
-
-                        for (uint256 ii = 0; ii < _publication.publishers.length; ii++) {
-                            if (_publication.publishers[ii] == publisher) {
-                                _publication.publishers[ii] = _publication.publishers[_publication.publishers.length - 1];
-                                _publication.publishers.pop();             
+                }   else {
+                        for (uint256 ii = 0; ii < _publication.identificationsOfPublisher[publisher].length; ii++) {
+                            if (_publication.identificationsOfPublisher[publisher][ii] == publicationIdentification) {
+                                _publication.identificationsOfPublisher[publisher][ii] = _publication.identificationsOfPublisher[publisher][_publication.identificationsOfPublisher[publisher].length - 1];
+                                _publication.identificationsOfPublisher[publisher].pop();             
                             }
                         }
-                    }   else {
-                            for (uint256 ii = 0; ii < _publication.identificationsOfPublisher[publisher].length; ii++) {
-                                if (_publication.identificationsOfPublisher[publisher][ii] == publicationIdentification) {
-                                    _publication.identificationsOfPublisher[publisher][ii] = _publication.identificationsOfPublisher[publisher][_publication.identificationsOfPublisher[publisher].length - 1];
-                                    _publication.identificationsOfPublisher[publisher].pop();             
-                                }
-                            }
-                    }
-                }        
+                }
+                      
             }   catch Error(string memory errorMessage) {
                     if (keccak256(abi.encodePacked(errorMessage)) == keccak256(abi.encodePacked(AIOMessage.METADATA_NOT_SENT))) {                    
                         revert(string.concat("article[", Strings.toString(i), "]: ", "Article is not published"));
